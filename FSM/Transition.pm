0a02babb1d9014b2a9fcc2d79acce7a529a5cf2c
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-02-09 11:30:40 +0100

    Improve objects

--- FSM/Transition.pm
+++ FSM/Transition.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use Mo qw(build is);
-use Mo::utils 0.28 qw(check_number_id);
+use Mo::utils 0.28 qw(check_code check_isa check_number_id);
 
 our $VERSION = 0.01;
 
@@ -27,8 +27,17 @@ has to => (
 sub BUILD {
 	my $self = shift;
 
+	# Check callback.
+	check_code($self, 'callback');
+
+	# Check from.
+	check_isa($self, 'from', 'Data::FSM::State');
+
 	# Check id.
 	check_number_id($self, 'id');
+
+	# Check to.
+	check_isa($self, 'to', 'Data::FSM::State');
 
 	return;
 }
