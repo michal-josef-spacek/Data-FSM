11055cbfb21355da8d64bea315386dcbb6aa0a3d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-02-09 11:44:20 +0100

    Add 'initial' parameter to State

--- FSM/State.pm
+++ FSM/State.pm
@@ -4,11 +4,15 @@ use strict;
 use warnings;
 
 use Mo qw(build is);
-use Mo::utils 0.28 qw(check_number_id);
+use Mo::utils 0.28 qw(check_bool check_number_id);
 
 our $VERSION = 0.01;
 
 has id => (
+	is => 'ro',
+);
+
+has initial => (
 	is => 'ro',
 );
 
@@ -21,6 +25,12 @@ sub BUILD {
 
 	# Check id.
 	check_number_id($self, 'id');
+
+	# Check inital.
+	if (! defined $self->{'inital'}) {
+		$self->{'inital'} = 0;
+	}
+	check_bool($self, 'inital');
 
 	# Check name.
 	# TODO
